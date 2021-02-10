b359863081722cbf5cf01316267059a989578f3b
arielferdman <feariel@gmail.com>
2021-02-11 01:55:34 +0200

    added ll alias to container provisioning script

--- ic.sh
+++ ic.sh
@@ -1,3 +1,6 @@
 container_id=$(docker ps | grep $1 | awk '{print $1}')
 docker cp init-container.sh $container_id:/usr/bin
 docker exec $container_id bash init-container.sh
+docker exec -i -u 0 $container_id bash <<EOF
+echo 'alias ll="ls -al"' >> /root/.bashrc
+EOF
