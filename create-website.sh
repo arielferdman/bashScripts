df6880d63836e74069e3cc2299c44ce1d4217a68
arielferdman <feariel@gmail.com>
2021-03-29 05:01:09 +0300

    added create website

--- create-website.sh
+++ create-website.sh
@@ -1,5 +1,4 @@
-touch styles.css
-touch main.js
+touch style.css
 
 cat <<EOF > index.html
 <!DOCTYPE html>
@@ -7,11 +6,21 @@ cat <<EOF > index.html
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="styles.css">
+	<link rel="stylesheet" href="style.css">
 	<title>Document</title>
 </head>
 <body>
 	<script src="main.js"></script>
 </body>
 </html>
+EOF
+
+cat <<EOF > main.js
+window.onload = () => {
+  start()  
+}
+
+function start() {
+
+}
 EOF
